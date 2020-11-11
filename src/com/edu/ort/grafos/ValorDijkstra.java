c470c9c975adee06399a100acb28a85427e245fc
Alexis <alexfosters2014@gmail.com>
2020-11-11 15:15:16 -0300

    mejora performance Dijkstra

---
+++ src/com/edu/ort/grafos/ValorDijkstra.java
@@ -0,0 +1,13 @@
+
+package com.edu.ort.grafos;
+
+public class ValorDijkstra {
+    public int[] dist;
+    public int [] ant;
+
+    public ValorDijkstra(int[] dist, int[] ant) {
+        this.dist = dist;
+        this.ant = ant;
+    }
+    
+}
